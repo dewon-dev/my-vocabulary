b768fcbfca07c26dea26133a4308aaf8a42a27bd
dewon-dev <ava.d.won@gmail.com>
2026-08-06 11:02:53 +0900

    Create first vocabulary data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
-
+const word = {
+  language: "일본어",
+  flag: "🇯🇵",
+  word: "勉強",
+  reading: "べんきょう",
+  meaning: "공부",
+}
 function App() {
   return (
-    <>
+    <div>
       <h1>My Vocabulary</h1>
-      <p>나만의 언어 노트</p>
-    </>
+
+<h2>{word.flag} {word.language}</h2>
+<h3>{word.word}</h3>
+<p>{word.reading}</p>
+<p>{word.meaning}</p>
+    </div>
   )
 }
 
