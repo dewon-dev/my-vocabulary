29c05b4e070d03700aedab939af038e337765ddd
dewon-dev <ava.d.won@gmail.com>
2026-08-06 11:14:24 +0900

    Add word list with map

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-const word = {
+const words = [{
+  
   language: "일본어",
   flag: "🇯🇵",
   word: "勉強",
   reading: "べんきょう",
   meaning: "공부",
-}
+}, 
+{
+  language: "일본어",
+  flag: "🇯🇵",
+  word: "猫",
+  reading: "ねこ",
+  meaning: "고양이",
+}, 
+{
+  language: "인도네시아어",
+  flag: "🇮🇩",
+  word: "terima kasih",
+  meaning: "고마워"
+},
+]
 function App() {
   return (
     <div>
       <h1>My Vocabulary</h1>
 
-<h2>{word.flag} {word.language}</h2>
-<h3>{word.word}</h3>
-<p>{word.reading}</p>
-<p>{word.meaning}</p>
+      {words.map((word) => (
+        <div>
+          <h2>{word.flag} {word.language}</h2>
+
+          <h3>{word.word}</h3>
+          <p>{word.reading}</p>
+          <p>{word.meaning}</p>
+        </div>
+      ))}
     </div>
   )
 }
